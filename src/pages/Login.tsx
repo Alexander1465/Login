bb190575dbd57ccfe80f759a69e8d280015e90f2
Alexander1465 <alexandervecin@gmail.com>
2024-10-23 18:01:26 +0100

    Actividad

---
+++ src/pages/Login.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+function Login() {
+ return (
+    <Container>
+        
+            <Typography variant='h1' color="primary">
+                Pagina Login de Alexander José Estupiñán Pérez
+            </Typography>
+            <Typography variant='h2' color="secondary">
+                Pagina Login de Alexander José Estupiñán Pérez
+            </Typography>
+            <Typography variant='h3' color="error">
+                Pagina Login de Alexander José Estupiñán Pérez
+            </Typography>
+            <Typography variant='subtitle1' color="success">
+                Pagina Login de Alexander José Estupiñán Pérez
+            </Typography>
+            <Typography variant='body1' color="primary">
+                Pagina Login de Alexander José Estupiñán Pérez
+            </Typography>
+        
+
+            <Button variant='contained' color='primary'>
+                Boton1
+            </Button>
+            <Button variant='text' color='secondary'>
+                Boton2
+            </Button>
+            <Button variant='outlined' color='error'>
+                Boton3
+            </Button>
+        
+    </Container>
+
+ )
+}
+
+
+export default Login;
